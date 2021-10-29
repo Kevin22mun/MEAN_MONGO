d688d87a793ffcda0bb0600ecf5f111c11204062
unknown <kevinrobertoms@gmail.com>
2021-10-28 19:29:53 -0500

    Creacion de Lista.html

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CrearProductoComponent } from './components/crear-producto/crear-producto.component';
+import { ListarProductosComponent } from './components/listar-productos/listar-productos.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    CrearProductoComponent
+    CrearProductoComponent,
+    ListarProductosComponent
   ],
   imports: [
     BrowserModule,
